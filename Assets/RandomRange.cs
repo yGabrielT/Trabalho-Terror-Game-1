c83e5ce6b74c605130db0e39aeacadd3ce0469e6
yToppu <95586355+yToppu@users.noreply.github.com>
2022-05-10 11:34:13 -0300

    Lanterna e obstáculos que se rotacionam

---
+++ Assets/RandomRange.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRange : MonoBehaviour
+{
+    public Vector3 rotateChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotateChange);
+    }
+}
